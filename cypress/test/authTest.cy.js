c13aae6e7ebef0b3a1336a962cf4a606eacb459d
SPowell1 <powellsukina@gmail.com>
2022-12-17 17:36:07 -0500

    Authentication Branch Update

--- cypress/test/authTest.cy.js
+++ cypress/test/authTest.cy.js
@@ -3,26 +3,6 @@ describe ('login/signup suite', ()=>{
         cy.visit('/')
     })
 
-    it('should signup with valid',()=>{
-
-        cy.get("#signInOrRegister").click();
-        
-        cy.origin(
-            "https://dev-mlluudmotpwoldtv.us.auth0.com",
-            { args: {} },
-            ({}) => {
-                
-                cy.get(`body > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > span:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)`).should('be.visible').click()
-                cy.get(`input[placeholder='yours@example.com']`).type("ghi@gmail.com");
-                cy.get(`input[placeholder='your password']`).type("Pass12rd", { log: false });
-                cy.get(".auth0-label-submit").click();
-            }
-        )
-       
-        cy.get(`.chakra-heading.css-kmq9po`).should('contain.text', 'Automation Camp Store')
-        cy.url().should('include','/products')
-    }),
-
      it('should login with valid',()=>{
 
         cy.get("#signInOrRegister").click();
@@ -57,9 +37,7 @@ describe ('login/signup suite', ()=>{
 
                 cy.get(`body > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)`).should('contain.text', 'Wrong')
             }
-        )
-       
-                
+        )          
     }),
 
     //Negative test for signup
